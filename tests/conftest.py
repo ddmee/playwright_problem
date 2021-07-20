87cbf807bf947016fdafb31d4c6c7deb4643a1e8
donal mee <mee.donal@gmail.com>
2021-07-20 14:55:09 +0100

    fix issue by waiting for server per max schmitt's tip

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,19 @@
 # third party
 from playwright.sync_api import Page
 import pytest
+import polling2
+import requests
 # local
 import web_server.commands_lib as commands_lib
+from test_lib.common_page import server_url
 import test_lib.login_page as login_page
 
+
+@pytest.fixture(scope='session', autouse=True)
+def wait_on_server():
+    """Server may not be available at first. Wait on it before running tests"""
+    polling2.poll(target=lambda: requests.get(server_url()).status_code == 200,
+                  step=1, timeout=30)
 
 @pytest.fixture()
 def sign_in_write_user(page: Page):
